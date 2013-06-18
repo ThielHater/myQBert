e072f9d205c7a8352ecf901d285678dd3a26775f
V-Modder <v-modder@web.de>
2013-06-18 15:04:15 +0200

    GamesStats & DirectX_Enviroment_Variables

--- GameStats.cpp
+++ GameStats.cpp
@@ -12,3 +12,47 @@ GameStats::GameStats(void)
 GameStats::~GameStats(void)
 {
 }
+
+int GameStats::GetLevel(void)
+{
+	return Level;
+}
+
+int GameStats::GetRound(void)
+{
+	return Round;
+}
+
+int GameStats::GetLifeCount(void)
+{
+	return LifeCount;
+}
+
+int GameStats::GetScore(void)
+{
+	return Score;
+}
+
+int GameStats::AddLevel(void)
+{
+	Level++;
+	return Level;
+}
+
+int GameStats::AddRound(void)
+{
+	Round++;
+	return Round;
+}
+
+int GameStats::SubLifeCount(void)
+{
+	LifeCount--;
+	return LifeCount;
+}
+
+int GameStats::AddScore(int Scr)
+{
+	Score += Scr;
+	return Score;
+}
--- GameStats.h
+++ GameStats.h
@@ -2,12 +2,22 @@
 
 class GameStats
 {
-	public:
-		GameStats(void);
-		~GameStats(void);
+	private:
 		int Level;
 		int Round;
 		int LifeCount;
 		int Score;
 		bool TimeFrozen; // Zeit wird pausiert, nur Q*Bert kann sich bewegen
+
+	public:
+		GameStats(void);
+		~GameStats(void);
+		int GetLevel(void);
+		int GetRound(void);
+		int GetLifeCount(void);
+		int GetScore(void);
+		int AddLevel(void);
+		int AddRound(void);
+		int SubLifeCount(void);
+		int AddScore(int);
 };
